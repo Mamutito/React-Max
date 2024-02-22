2fd5364e7c2d924521db4180ed70dfd2e3cdd36a
Mamutito <aithor123@gmail.com>
2024-02-22 13:55:57 +0000

    feat: react events handlers

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,6 +4,7 @@ import CoreConcepts from "./components/CoreConcepts";
 import TabButton from "./components/TabButton";
 
 function App() {
+  function handleSelect() {}
   return (
     <div>
       <Header />
@@ -19,10 +20,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
       </main>
