d26bb64eecbf7c585571924b02a1ace6ed677880
Mamutito <aithor123@gmail.com>
2024-02-28 11:01:30 +0000

    feat: wIP gameboard component

---
+++ 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function GameBoard() {
+  const [GameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectedSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+    return;
+  };
+  return (
+    <ol id="game-board">
+      {GameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => handleSelectedSquare(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default GameBoard;
