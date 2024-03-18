a8b4182d88a2feb3e05c6d51b142ccc3f8277f62
Mamutito <aithor123@gmail.com>
2024-03-18 14:57:57 +0000

    feat: first quiz component implementation

---
+++ 09-interactive-quiz/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import quizLogo from "../assets/quiz-logo.png";
+const Header = () => {
+  return (
+    <header>
+      <img src={quizLogo} alt="Quiz logo" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+};
+
+export default Header;
