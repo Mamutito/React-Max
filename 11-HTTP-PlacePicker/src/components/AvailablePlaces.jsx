2eb0130d4ec55a8cc25dd258fb6c9fcda3f84495
Mamutito <aithor123@gmail.com>
2024-03-26 14:02:58 +0000

    feat: fetch places from our backend

--- 11-HTTP-PlacePicker/src/components/AvailablePlaces.jsx
+++ 11-HTTP-PlacePicker/src/components/AvailablePlaces.jsx
@@ -1,10 +1,26 @@
-import Places from './Places.jsx';
+import React, { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    };
+    fetchPlaces();
+    /*     fetch("http://localhost:3000/places")
+      .then((response) => response.json())
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      }); */
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
