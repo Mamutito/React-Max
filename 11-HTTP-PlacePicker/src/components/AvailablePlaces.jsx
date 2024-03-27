ded4de63137ce157e55d921da64bc33f2b68a5ae
Mamutito <aithor123@gmail.com>
2024-03-27 12:11:51 +0000

    feat: add PUT and GET utily functions

--- 11-HTTP-PlacePicker/src/components/AvailablePlaces.jsx
+++ 11-HTTP-PlacePicker/src/components/AvailablePlaces.jsx
@@ -1,13 +1,36 @@
 import React, { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import ErrorMessage from "./ErrorMessage.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
   useEffect(() => {
     const fetchPlaces = async () => {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsLoading(true);
+      try {
+        const places = await fetchAvailablePlaces();
+        navigator.geolocation.getCurrentPosition(({ coords }) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            coords.latitude,
+            coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsLoading(false);
+        });
+      } catch (error) {
+        console.log(error);
+        setError({
+          message:
+            error.message || "Could not Fetch places, please try again later.",
+        });
+        setIsLoading(false);
+      }
     };
     fetchPlaces();
     /*     fetch("http://localhost:3000/places")
@@ -17,12 +40,17 @@ export default function AvailablePlaces({ onSelectPlace }) {
       }); */
   }, []);
 
+  if (error) {
+    return <ErrorMessage title="An error occurred!" message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isLoading}
+      loadingText="Fetching place data..."
     />
   );
 }
