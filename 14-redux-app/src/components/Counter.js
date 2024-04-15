5662542813eb0b49d29d46630fab1e60bd27fb38
Mamutito <aithor123@gmail.com>
2024-04-15 15:35:30 +0100

    feat: add auth redux store

--- 14-redux-app/src/components/Counter.js
+++ 14-redux-app/src/components/Counter.js
@@ -1,10 +1,10 @@
-import { counterActions } from "../store";
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
   const dispatch = useDispatch();
   const toggleCounterHandler = () => dispatch(counterActions.toggleCounter());
   const handleIncrement = () => dispatch(counterActions.increment());
