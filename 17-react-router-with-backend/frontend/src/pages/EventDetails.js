d080bd31612c6119b345601c093a00f5fc027cc0
Mamutito <aithor123@gmail.com>
2024-04-25 14:50:31 +0100

    feat: delete event handler and submitting state

--- 17-react-router-with-backend/frontend/src/pages/EventDetails.js
+++ 17-react-router-with-backend/frontend/src/pages/EventDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 function EventDetailsPage() {
@@ -22,7 +22,17 @@ export const loader = async ({ params }) => {
       { message: "Could not get the event selected" },
       { status: 500 }
     );
-  } else {
-    return response;
   }
+  return response;
+};
+
+export const action = async ({ params, request }) => {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id, {
+    method: request.method,
+  });
+  if (!response.ok) {
+    throw json({ message: "Could not delete the event" }, { status: 500 });
+  }
+  return redirect("/events");
 };
