93b6b045486b8f13cf1ada9c58dc41c2b18e21b7
Mamutito <aithor123@gmail.com>
2024-05-06 14:38:05 +0100

    feat: create new events

--- 18-react-query/src/components/Events/NewEvent.jsx
+++ 18-react-query/src/components/Events/NewEvent.jsx
@@ -1,24 +1,49 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { client, createNewEvent } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { isPending, mutate, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
+      {isError && (
+        <ErrorBlock
+          title="Failed to created event"
+          message={
+            error.info?.message ||
+            "Failed to create event. Please check your inputs and try again later."
+          }
+        />
+      )}
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting..."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
     </Modal>
   );
--- 18-react-query/src/utils/http.js
+++ 18-react-query/src/utils/http.js
@@ -1,3 +1,6 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const client = new QueryClient();
 export async function fetchEvents({ signal, searchTerm }) {
   let url = "http://localhost:3000/events";
   if (searchTerm) {
@@ -15,4 +18,40 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+export async function createNewEvent(eventData) {
+  const response = await fetch("http://localhost:3000/events", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the events");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { events } = await response.json();
+
+  return events;
+}
+
+export async function fetchImages({ signal }) {
+  const response = await fetch("http://localhost:3000/events/images", {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the images");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
 }
