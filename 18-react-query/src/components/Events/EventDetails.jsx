ff237e04b89c854f5052fec63efa0a769c42f10a
Mamutito <aithor123@gmail.com>
2024-05-06 18:44:52 +0100

    refactor: enhancing Event details

--- 18-react-query/src/components/Events/EventDetails.jsx
+++ 18-react-query/src/components/Events/EventDetails.jsx
@@ -13,6 +13,59 @@ export default function EventDetails() {
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <LoadingIndicator />;
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="Failed to load event"
+          message={error.info?.message || "Please try again later."}
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedData = new Date(data.date).toLocaleDateString("en-ES", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <Link to="delete">Delete</Link>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.title}</p>
+              <time dateTime={`${data.date}T${data.time}`}>
+                {formattedData} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+        ;
+      </>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -28,30 +81,7 @@ export default function EventDetails() {
           message={error.info?.message || "Please try again later."}
         />
       )}
-      {data && (
-        <article id="event-details">
-          <header>
-            <h1>{data.title}</h1>
-            <nav>
-              <Link to="delete">Delete</Link>
-              <Link to="edit">Edit</Link>
-            </nav>
-          </header>
-
-          <div id="event-details-content">
-            <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
-            <div id="event-details-info">
-              <div>
-                <p id="event-details-location">{data.title}</p>
-                <time dateTime={`${data.date}T${data.time}`}>
-                  {data.date} @ {data.time}
-                </time>
-              </div>
-              <p id="event-details-description">{data.description}</p>
-            </div>
-          </div>
-        </article>
-      )}
+      {data && <article id="event-details">{content}</article>}
     </>
   );
 }
